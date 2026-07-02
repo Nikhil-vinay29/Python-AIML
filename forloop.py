ee88238992f44b8892f6aa4c8632cdcdf04b66ff
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-07-02 18:02:41 +0530

    forloop

---
+++ forloop.py
@@ -0,0 +1,14 @@
+# This is a simple for loop that prints numbers from 1 to 10.
+for i in range(1, 11):
+    print(i)
+# This is a simple for loop that prints numbers from 10 to 1 in reverse order.
+for i in range(10, 0, -1):
+    print(i)
+# This is a simple for loop that prints even numbers from 2 to 50.
+for i in range(2, 51, 2):
+    print(i)   
+
+# This is a simple for loop that prints odd numbers from 1 to 49.
+for i in range(1, 50, 2):
+    print(i)    
+
