02bcb9a7108658cd5bb1395abe2239bece5e8aab
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-07-04 22:08:24 +0530

    forloop

--- forloop.py
+++ forloop.py
@@ -35,3 +35,43 @@ for i in range(n):
     c = a + b
     a = b
     b = c
+
+# This is a simple for loop that calculates the sum of squares of numbers from 1 to n entered by the user.
+
+n = int(input("Enter N: "))
+
+sum = 0
+
+for i in range(1, n + 1):
+    sum = sum + (i * i)
+
+print("Sum of Squares =", sum)
+
+# This is a simple for loop that prints the common factors of two numbers entered by the user.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Common Factors are:")
+
+for i in range(1, min(a, b) + 1):
+    if a % i == 0 and b % i == 0:
+        print(i)
+
+# This is a simple for loop that counts the number of prime numbers up to n entered by the user.
+n = int(input("Enter N: "))
+
+count = 0
+
+for i in range(2, n + 1):
+    prime = True
+
+    for j in range(2, i):
+        if i % j == 0:
+            prime = False
+            break
+
+    if prime:
+        count = count + 1
+
+print("Total Prime Numbers =", count)
