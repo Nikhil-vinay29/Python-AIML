3aa4c5d60ce9e0cccc6ced01310311978a87941d
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-07-08 11:20:41 +0530

    setbasics

---
+++ setbasics.py
@@ -0,0 +1,28 @@
+# Create a set with unique elements
+numbers = {10, 20, 30, 40}
+
+print(numbers)
+
+# Create a set with duplicate elements
+numbers = {10, 20, 20, 30, 30, 30}
+
+print(numbers)
+
+# Create an empty set
+numbers = set()
+
+print(numbers)
+
+# Add an element to the set
+numbers = {10, 20, 30}
+
+numbers.add(40)
+
+print(numbers)
+
+# Update the set with multiple elements
+numbers = {10, 20, 30}
+
+numbers.update([40, 50, 60])
+
+print(numbers)
