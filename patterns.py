df43708277e157e5f05c74ee6332bedce6e97c8c
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-02 23:30:46 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -15,3 +15,12 @@ for i in range(1, n + 1):
     for j in range(i):
         print("*", end=" ")
     print()
+
+# Pattern 3: Decreasing Star Triangle
+
+n = 5
+
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
