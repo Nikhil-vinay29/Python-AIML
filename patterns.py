13683c36b44cc4d010d6ef62cf4d1b07eb908681
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-03 22:57:25 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -24,3 +24,13 @@ for i in range(n, 0, -1):
     for j in range(i):
         print("*", end=" ")
     print()
+
+
+# Pattern 4: Number Triangle
+
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
