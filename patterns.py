3036a55d5893d27e4c238d83604b183c04541f4a
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-05 22:34:06 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -44,3 +44,12 @@ for i in range(1, n + 1):
     for j in range(i):
         print(i, end=" ")
     print()
+
+# Pattern 6: Alphabet Triangle
+
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(chr(65 + j), end=" ")
+    print()
