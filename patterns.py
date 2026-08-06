df80c485bd287c0c72bc0c63191315893621c9a1
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-06 23:26:56 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -53,3 +53,18 @@ for i in range(1, n + 1):
     for j in range(i):
         print(chr(65 + j), end=" ")
     print()
+
+
+# Pattern 7: Right-Aligned Star Triangle
+
+n = 5
+
+for i in range(1, n + 1):
+
+    for j in range(n - i):
+        print(" ", end=" ")
+
+    for k in range(i):
+        print("*", end=" ")
+
+    print()
