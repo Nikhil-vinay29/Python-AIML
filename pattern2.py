3b606e8fe56c37ed0e2d833203604bd67e13363b
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-08 23:20:21 +0530

    patterns2

--- pattern2.py
+++ pattern2.py
@@ -13,3 +13,20 @@ for i in range(1, n + 1):
         print("*", end=" ")
 
     print()
+
+
+# Pattern 9: Inverted Pyramid
+
+n = 5
+
+for i in range(n, 0, -1):
+
+    # Print spaces
+    for j in range(n - i):
+        print(" ", end=" ")
+
+    # Print stars
+    for k in range(2 * i - 1):
+        print("*", end=" ")
+
+    print()
