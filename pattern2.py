d2a021cf85daf8e0a75d10014cea58f2b958865f
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-11 23:25:38 +0530

    patterns2

--- pattern2.py
+++ pattern2.py
@@ -12,10 +12,7 @@ for i in range(1, n + 1):
     for k in range(2 * i - 1):
         print("*", end=" ")
 
-    print()
-
-
-# Pattern 9: Inverted Pyramid
+    print()# Pattern 9: Inverted Pyramid
 
 n = 5
 
@@ -31,27 +28,42 @@ for i in range(n, 0, -1):
 
     print()
 
-
 # Pattern 10: Diamond Star Pattern
 
 n = 5
 
 # Upper half
 for i in range(1, n + 1):
+
     for j in range(n - i):
         print(" ", end=" ")
-    
+
     for k in range(2 * i - 1):
         print("*", end=" ")
-    
+
     print()
 
 # Lower half
 for i in range(n - 1, 0, -1):
+
     for j in range(n - i):
         print(" ", end=" ")
-    
+
     for k in range(2 * i - 1):
         print("*", end=" ")
-    
+
+    print()
+
+# Pattern 11: Number Pyramid
+
+n = 5
+
+for i in range(1, n + 1):
+
+    for j in range(n - i):
+        print(" ", end=" ")
+
+    for k in range(1, i + 1):
+        print(k, end=" ")
+
     print()
