14c44e69e65f3a5755d4172419526a1135530979
nikhil-vinay29 <nikhilmorampudi29@gmail.com>
2026-08-12 23:23:47 +0530

    patterns2

--- pattern2.py
+++ pattern2.py
@@ -67,3 +67,18 @@ for i in range(1, n + 1):
         print(k, end=" ")
 
     print()
+
+
+# Pattern 12: Repeated Number Pyramid
+
+n = 5
+
+for i in range(1, n + 1):
+
+    for j in range(n - i):
+        print(" ", end=" ")
+
+    for k in range(2 * i - 1):
+        print(i, end=" ")
+
+    print()
